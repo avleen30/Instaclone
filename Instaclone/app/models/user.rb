55574c97e9aa9e10096acdc8ca65d2593578667f
Avleen <avleen3@gmail.com>
2017-10-20 22:01:02 +0000

    adding user names

--- Instaclone/app/models/user.rb
+++ Instaclone/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+
+  has_many :posts, dependent: :destroy
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   validates :user_name, presence: true, length: { minimum: 4, maximum: 16 }
