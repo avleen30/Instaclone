6b780c152bc1c95b53f2fc3d3cf31e05f92b4173
Avleen <avleen3@gmail.com>
2017-10-22 22:27:13 +0000

    styling registration form

--- Instaclone/app/controllers/comments_controller.rb
+++ Instaclone/app/controllers/comments_controller.rb
@@ -7,8 +7,11 @@ class CommentsController < ApplicationController
     @comment.user_id = current_user.id
 
     if @comment.save
-      flash[:success] = "You commented the hell out of that post!"
-      redirect_to :back
+      create_notification @post, @comment
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
 
     else
       flash[:alert] = "Check the comment form, something went horribly wrong."
@@ -18,9 +21,14 @@ class CommentsController < ApplicationController
 
   def destroy
     @comment = @post.comments.find(params[:id])
-    @comment.destroy
-    flash[:success] = "Comment deleted :("
-    redirect_to root_path
+
+    if @comment.user_id == current_user.id
+      @comment.destroy
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js
+      end
+    end
   end
 
   private
